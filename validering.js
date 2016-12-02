a679170d1aea16fae449472fbf1d1c06d35f2d89
noisyy <conradz@hotmail.no>
2016-12-02 12:19:26 +0100

    3 store bokstaver

--- validering.js
+++ validering.js
@@ -3,43 +3,45 @@ function fjernMelding()
 	document.getElementById("melding1").innerHTML="";
 }
 
-
 function validering1() {
 
-	var flyplasskode=("flyplasskode");
-	var tegn1, tegn2, tegn3;
-	var lovligFlyplasskode=true;
+    	var flyplasskode = document.getElementById("flyplasskode").value;
+			var flyplassnavn = document.getElementById("flyplassnavn").value;
 
-	if(!flyplasskode)
+			var tegn1, tegn2, tegn3;
+			var lovligFlyplasskode=true;
+
+			if(!flyplasskode)
+			{
+				lovligFlyplasskode=false;
+				feilmelding=feilmelding+"Flyplasskode er ikke fylt ut</br>";
+			}
+
+			else if(flyplasskode.length !=3)
+			{
+				lovligFlyplasskode=false;
+				feilmelding = feilmelding + "Flyplasskode skal bestå av 3 store bokstaver <br>";
+			}
+
+			else
+			{
+				tegn1=flyplasskode[0]; /*tegn1=flyplasskode.substr(0,1);*/
+				tegn2=flyplasskode[1]; /*tegn2=flyplasskode.substr(1,1);*/
+				tegn3=flyplasskode[2]; /*tegn2=flyplasskode.substr(2,1);*/
+			}
+
+			if (tegn1 < "A" || tegn1 > "Z" || tegn2 < "A" || tegn2 > "Z" || tegn3 < "A" || tegn3 > "Z")
+			{
+				lovligFlyplasskode=false;
+				feilmelding = feilmelding + "Flyplasskode skal bestå av 3 store bokstaver <br>";
+			}
+
+
+    if (flyplasskode && flyplassnavn)
 	{
-		lovligFlyplasskode=false;
-		document.write("Flyplasskode er ikke fylt ut");
-	}
-
-	else if(flyplasskode.length !=3)
-	{
-		lovligFlyplasskode=false;
-		document.write("Flyplasskode skal bestå av 3 store bokstaver");
-	}
-
-	else
-	{
-		tegn1=flyplasskode[0]; /*tegn1=flyplasskode.substr(0,1);*/
-		tegn2=flyplasskode[1]; /*tegn2=flyplasskode.substr(1,1);*/
-		tegn3=flyplasskode[2]; /*tegn2=flyplasskode.substr(2,1);*/
-	}
-
-	if (tegn1 < "A" || tegn1 > "Z" || tegn2 < "A" || tegn2 > "Z" || tegn3 < "A" || tegn3 > "Z")
-	{
-		lovligFlyplasskode=false;
-		document.write("Flyplasskode skal bestå av 3 store bokstaver");
-	}
-
-	if(lovligFlyplasskode)
-	{
-		document.write("Flyplasskode er korrekt fylt ut");
-	}
-
+        return true;
+        document.getElementById("melding1").innerHTML = "Alt er riktig fylt ut";
+    }
 
 	else
 	{
