c50ccc06fff44ff1a651cc8ab3413dd56ef97157
noisyy <conradz@hotmail.no>
2016-12-02 15:22:13 +0100

    p

--- js/validering.js
+++ js/validering.js
@@ -55,3 +55,55 @@ function validering1() {
     }
 
   }
+
+
+
+  function validering1() {
+
+      var flyplasskode = document.getElementById("flyplasskode").value;
+      var flyplassnavn = document.getElementById("flyplassnavn").value;
+
+      var feilmelding = "";
+
+      if (!flyplasskode) {
+          feilmelding = feilmelding + "Flyplasskode er ikke fylt ut <br />";
+      }
+
+      if(!flyplasskode)
+      {
+        lovligFlyplasskode=false;
+        feilmelding = feilmelding + "Flyplasskode er ikke fylt ut";
+      }
+
+      else if(flyplasskode.length !=3)
+      {
+        lovligFlyplasskode=false;
+        feilmelding = feilmelding + "Flyplasskode skal bestå av kun 3 tegn";
+      }
+
+      else
+      {
+        tegn1=flyplasskode[0];
+        tegn2=flyplasskode[1];
+        tegn3=flyplasskode[2];
+      }
+
+      if (tegn1 < "A" || tegn1 > "Z" || tegn2 < "A" || tegn2 > "Z" || tegn3 < "A" || tegn3 > "Z")
+      {
+        lovligFlyplasskode=false;
+        feilmelding = feilmelding + "Flyplasskode innholder ulovlige tegn";
+      }
+
+      if (!flyplassnavn) {
+          feilmelding = feilmelding + "Flyplassnavn er ikke fylt ut <br />";
+      }
+
+      if (flyplasskode && flyplassnavn) {
+          return true;
+          document.getElementById("melding1").innerHTML = "Alt er riktig fylt ut";
+      } else {
+          document.getElementById("melding1").style.color = "red";
+          document.getElementById("melding1").innerHTML = feilmelding;
+          return false;
+      }
+  }
