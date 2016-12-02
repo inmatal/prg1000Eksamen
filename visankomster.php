c1e406999973f773a51036817323881d619b6d3d
Carl <mosby2@hotmail.com>
2016-12-02 16:11:53 +0100

    ø

--- visankomster.php
+++ visankomster.php
@@ -1,7 +1,7 @@
 <!DOCTYPE=HTML>
 <html lang="no">
 <meta charset="UTF-8">
-<link rel="stylesheet" type="text/css" href="stil.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
    <head>
      <Title>Søk</title>
 	 <script src="js/hendelser.js"></script>
@@ -10,7 +10,7 @@
 <body>
 
 <form method="post" onsubmit="return validering2()">
-Flyplass <input type="text" id="klassekode" name="klassekode" onfocus="farge(this)" onblur="ikkefarge(this)" onKeyUp="vis(this.value)" required/> <br />
+Klassesøk <input type="text" id="klassekode" name="klassekode" onfocus="farge(this)" onblur="ikkefarge(this)" onmouseover="mouseover(this)" onmouseout="mouseout(this)" onKeyUp="vis(this.value)" required/> <br />
 <input type="submit" value="fortsett" id="fortsett" name="fortsett"/> <br />
 <input type="reset" value="nullstill" id="nullstill" name="nullstill" onClick="fjernMelding()"/> <br />
 
@@ -21,58 +21,80 @@ Flyplass <input type="text" id="klassekode" name="klassekode" onfocus="farge(thi
 
 @$fortsett=$_POST["fortsett"];
 
-if ($fortsett) {
-    $klassekode=$_POST["klassekode"];
-    $klassekode=trim($klassekode); /*trim fjerner mellomrom først og sist i tekst strengen*/
+if ($fortsett)
+{
+
+$klassekode=$_POST["klassekode"];
+$klassekode=trim($klassekode); /*trim fjerner mellomrom først og sist i tekst strengen*/
 $klassekode=strtoupper($klassekode);
-    $lovligKlassekode=true;
+$lovligKlassekode=true;
 
-    if (!$klassekode) {
-        /*print("klassekode ikke fylt ut");*/
-    $lovligKlassekode=false;
-    } elseif (strlen($klassekode)!=3) {
-        /*print("klassekode består ikke av 3 tegn");*/
-    $lovligKlassekode=false;
-    } else {
-        $tegn1=$klassekode[0];
-        $tegn2=$klassekode[1];
-        $tegn3=$klassekode[2];
+if(!$klassekode)
+{
+	/*print("klassekode ikke fylt ut");*/
+	$lovligKlassekode=false;
+}
+
+else if(strlen($klassekode)!=3)
+{
+	/*print("klassekode består ikke av 3 tegn");*/
+	$lovligKlassekode=false;
+}
+
+else
+{
+	$tegn1=$klassekode[0];
+	$tegn2=$klassekode[1];
+	$tegn3=$klassekode[2];
 
 
-        if ($tegn1<"A" || $tegn1>"Z" || $tegn2<"A" || $tegn2>"Z" || $tegn3<"1" || $tegn3>"9") {
-            /*print("her er det noe muffins");*/
-        $lovligKlassekode=false;
-        }
-    }
+	if($tegn1<"A" || $tegn1>"Z" || $tegn2<"A" || $tegn2>"Z" || $tegn3<"1" || $tegn3>"9")
+	{
+		/*print("her er det noe muffins");*/
+		$lovligKlassekode=false;
+	}
+}
 
 
 
-    $filnavn="D:\\Sites//home.hbv.no/phptemp/060929/student.txt";
-    $filoperasjon="r";
+$filnavn="D:\\Sites//home.hbv.no/phptemp/060929/student.txt";
+$filoperasjon="r";
 
 /*print ("<h2> Studenter </h2> </br>");*/
 
 $fil=fopen($filnavn, $filoperasjon);
 
 
-    while ($linje=fgets($fil)) {
-        if ($linje !="") {
-            $del=explode(";", $linje);
-            $sokeord=trim($del[3]);
+while($linje=fgets($fil))
 
-            if ($sokeord==$klassekode) {
-                $brukernavn=trim($del[0]);
-                $fornavn=trim($del[1]);
-                $etternavn=trim($del[2]);
-                $klasskode=trim($del[3]);
+	{
 
-                print("Brukernavn: $brukernavn </br> Navn: $fornavn $etternavn </br> Klasse: $klasskode <br><br>");
-            }
-        }
-    }
+	if ($linje !="")
+
+	{
+
+	$del=explode(";", $linje);
+	$sokeord=trim($del[3]);
+
+	if($sokeord==$klassekode)
+		{
+
+		$brukernavn=trim($del[0]);
+		$fornavn=trim($del[1]);
+		$etternavn=trim($del[2]);
+		$klasskode=trim($del[3]);
+
+		print("Brukernavn: $brukernavn </br> Navn: $fornavn $etternavn </br> Klasse: $klasskode <br><br>");
+
+		}
 
 
-    fclose($fil);
+	}
+
+}
+
+
+fclose($fil);
 }
 ?>
 <div id="melding2"></div>
