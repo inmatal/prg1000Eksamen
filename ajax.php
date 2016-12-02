6f78c6125e779c57924ee07f4f88e05135cd24d5
staarvik <Staarvik@ip-1261.hive.no>
2016-12-02 13:39:16 +0100

    head
    valg

--- ajax.php
+++ ajax.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 $delflyplasskode=$_GET["flyplasskode"];
 $filnavn="D:\\Sites\\home.hbv.no\\phptemp\\web-prg10v06/flygning.txt";
@@ -25,31 +25,3 @@ while ($linje=fgets($fil)) {
 print("</table>");
 fclose($fil);
 ?>
-=======
-<?php
-$delflyplasskode=$_GET["flyplasskode"];
-$filnavn="D:\\Sites\\home.hbv.no\\phptemp\\web-prg10v06/flygning.txt";
-$filoperasjon="r";
-$fil= fopen($filnavn, $filoperasjon);
-
-print ("<table>");
-
-while ($linje=fgets($fil)) {
-    if ($linje !="") {
-        $del=explode("  ", $linje);
-        $flightnr=$del[0];
-        $fra=$del[1];
-        $til=$del[2];
-        $dato=$del[3];
-
-        $startpos=stripos($fra, $delflyplasskode);
-        if ($startpos!==false) {
-            print("<tr><td>$fra</td><td>$til</tr></td><br>");
-        }
-    }
-}
-
-print("</table>");
-fclose($fil);
-?>
->>>>>>> origin/master
