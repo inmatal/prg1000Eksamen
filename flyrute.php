5908de3bcc4c5d279b556f8ea0aab6e455631c81
staarvik <andreast11@live.no>
2016-12-03 17:05:36 +0100

    magnus

--- flyrute.php
+++ flyrute.php
@@ -5,11 +5,42 @@
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="css/design.css">
         <title>Registrer Flyrute</title>
         <script src="js/hendelser.js"></script>
         <script src="js/validering.js"></script>
     </head>
     <body>
+
+      <center><header>Eksamen</header></center>
+
+    <div id="nav">
+      <ul>
+        <li><a href="index.html">Gruppe 6 </a> </li>
+        <li><a href="flyplass.php">Registrere flyplass </a> </li>
+          <li><a href="visflyplass.php">Vise flyplasser</a> </li>
+            <li><a href="flyrute.php">Registrere flyrute </a> </li>
+              <li><a href="visflyrute.php">Vise flyruter</a> </li>
+                <li><a href="flygning.php">Registrere flygninger</a> </li>
+                  <li><a href="visflygninger.php">Vise flygninger</a> </li>
+                    <li><a href="visavgang.php">Vise avganger</a> </li>
+                      <li><a href="visankomster.php">Vise ankomster</a> </li>
+
+      </ul>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
         <form method="post" onSubmit="return validering1()">
             <div class="tooltip">
                 Avganger <input type="text" id="avganger" name="avganger" onfocus="farge(this)" onblur="ikkefarge(this)" onKeyUp="vis(this.value)"/>
@@ -48,5 +79,11 @@
 
         <div id="melding1"></div>
         <div id="melding"></div>
+
+
+
     </body>
+    <footer>
+    <h4>Laget av gruppe 6, som består av:</h4>
+    </footer>
 </html>
