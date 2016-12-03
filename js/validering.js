9dde29e8fcc01941d7b95225503430fb72bfe96b
Carl <mosby2@hotmail.com>
2016-12-03 13:08:47 +0100

    js

--- js/validering.js
+++ js/validering.js
@@ -1,7 +1,5 @@
 function validering() {
     var flyplasskode = document.getElementById("flyplasskode").value;
-    /*var fra = document.getElementById("fra").value;
-    var til = document.getElementById("til").value;*/
     var tegn1,
         tegn2,
         tegn3;
@@ -14,15 +12,7 @@ function validering() {
     if (!regexToUse.test(flyplasskode)) {
         lovligflyplasskode = false;
         feilmelding = feilmelding + "Flyplasskode er ikke fylt ut riktig<br />";
-    }/*
-    if (!regexToUse.test(fra)) {
-        lovligflyplasskode = false;
-        feilmelding = feilmelding + "Flyplasskode er ikke fylt ut riktig<br />";
     }
-    if (!regexToUse.test(til)) {
-        lovligflyplasskode = false;
-        feilmelding = feilmelding + "Flyplasskode er ikke fylt ut riktig<br />";
-    }*/
 
     if (lovligflyplasskode) {
         return true;
@@ -37,3 +27,34 @@ function validering() {
         document.write("Flyplasskode er korrekt fylt ut ")
     }
   }
+
+  function validering1() {
+      var fra = document.getElementById("fra").value;
+      var tegn1,
+          tegn2,
+          tegn3;
+      var lovligfra = true;
+
+      var feilmelding = "";
+
+      var regexToUse = /^[a-zA-Z]{3}$/;
+
+      if (!regexToUse.test(fra)) {
+          lovligfra = false;
+          feilmelding = feilmelding + "Flyplasskode er ikke fylt ut riktig<br />";
+      }
+
+  
+      if (lovligfra) {
+          return true;
+          document.getElementById("melding1").innerHTML = "Alt er riktig fylt ut";
+      } else {
+          document.getElementById("melding1").style.color = "red";
+          document.getElementById("melding1").innerHTML = feilmelding;
+          return false;
+      }
+
+      if (fra) {
+          document.write("Flyplasskode er korrekt fylt ut ")
+      }
+    }
