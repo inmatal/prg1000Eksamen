a855c6be29b7ca2989ac289ebe3e8428a33ceec7
staarvik <andreast11@live.no>
2016-12-03 17:08:28 +0100

    Magnus

--- visflyrute.php
+++ visflyrute.php
@@ -3,12 +3,39 @@
 <html lang="no">
 
 <meta charset="UTF-8">
+  <link rel="stylesheet" href="css/design.css">
 
 <head>
 <title>Vis Flyruter</title>
 </head>
 
 <body>
+
+
+	<center><header>Eksamen</header></center>
+
+<div id="nav">
+  <ul>
+    <li><a href="index.html">Gruppe 6 </a> </li>
+    <li><a href="flyplass.php">Registrere flyplass </a> </li>
+      <li><a href="visflyplass.php">Vise flyplasser</a> </li>
+        <li><a href="flyrute.php">Registrere flyrute </a> </li>
+          <li><a href="visflyrute.php">Vise flyruter</a> </li>
+            <li><a href="flygning.php">Registrere flygninger</a> </li>
+              <li><a href="visflygninger.php">Vise flygninger</a> </li>
+                <li><a href="visavgang.php">Vise avganger</a> </li>
+                  <li><a href="visankomster.php">Vise ankomster</a> </li>
+
+  </ul>
+</div>
+
+
+
+
+
+
+
+
 
 <form method="POST">
 
@@ -51,4 +78,7 @@ print("</table>");
 ?>
 
 </body>
+<footer>
+<h4>Laget av gruppe 6, som består av:</h4>
+</footer>
 </html>
