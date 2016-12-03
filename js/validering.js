c8f689ba995a479aa7acc1145643f97d376f4281
Carl <mosby2@hotmail.com>
2016-12-03 13:28:20 +0100

    l

--- js/validering.js
+++ js/validering.js
@@ -39,19 +39,18 @@ function validering() {
       var tegn1,
           tegn2,
           tegn3;
-      var lovligfra = true;
-      var lovligtil = true;
+      var lovligFra = true;
 
       var feilmelding = "";
 
       var regexToUse = /^[a-zA-Z]{3}$/;
 
       if (!regexToUse.test(fra)) {
-          lovligfra = false;
+          lovligFra = false;
           feilmelding = feilmelding + "Flyplasskode er ikke fylt ut riktig<br />";
       }
       if (!regexToUse.test(til)) {
-          lovligtil = false;
+          lovligFra = false;
           feilmelding = feilmelding + "Flyplasskode er ikke fylt ut riktig<br />";
       }
 
@@ -64,13 +63,7 @@ function validering() {
           document.getElementById("melding1").innerHTML = feilmelding;
           return false;
       }
-      if (lovligtil) {
-          return true;
-          document.getElementById("melding1").innerHTML = "Alt er riktig fylt ut";
-      } else {
-          document.getElementById("melding1").style.color = "red";
-          document.getElementById("melding1").innerHTML = feilmelding;
-          return false;
+
       }
 
       if (fra) {
