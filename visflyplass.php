b6e7558cfd8583ee3ee8cb46ee9113a1834bfc3f
staarvik <andreast11@live.no>
2016-12-03 17:00:40 +0100

    hei

--- visflyplass.php
+++ visflyplass.php
@@ -3,12 +3,33 @@
 <html lang="no">
 
 <meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="css/design.css">
 
 <head>
 <title>Vis Flyplasser</title>
 </head>
 
 <body>
+
+	<center><header>Eksamen</header></center>
+
+<div id="nav">
+  <ul>
+    <li><a href="index.html">Gruppe 6 </a> </li>
+    <li><a href="flyplass.php">Registrere flyplass </a> </li>
+      <li><a href="visflyplass.php">Vise flyplasser</a> </li>
+        <li><a href="flyrute.php">Registrere flyrute </a> </li>
+          <li><a href="visflyrute.php">Vise flyruter</a> </li>
+            <li><a href="flygning.php">Registrere flygninger</a> </li>
+              <li><a href="visflygninger.php">Vise flygninger</a> </li>
+                <li><a href="visavgang.php">Vise avganger</a> </li>
+                  <li><a href="visankomster.php">Vise ankomster</a> </li>
+
+  </ul>
+</div>
+
+
+
 
 <form method="POST">
 
@@ -50,5 +71,9 @@ if ($linje !="") {
 print("</table>");
 ?>
 
+
+<footer>
+<h4>Laget av gruppe 6, som består av:</h4>
+</footer>
 </body>
 </html>
