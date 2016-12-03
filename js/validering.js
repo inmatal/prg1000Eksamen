c9e4e9957468baabcafe522b77524eaca952917f
Carl <mosby2@hotmail.com>
2016-12-03 13:17:09 +0100

    l

--- js/validering.js
+++ js/validering.js
@@ -30,10 +30,12 @@ function validering() {
 
   function validering1() {
       var fra = document.getElementById("fra").value;
+      var til = document.getElementById("til").value;
       var tegn1,
           tegn2,
           tegn3;
       var lovligfra = true;
+      var lovligtil = true;
 
       var feilmelding = "";
 
@@ -43,9 +45,21 @@ function validering() {
           lovligfra = false;
           feilmelding = feilmelding + "Flyplasskode er ikke fylt ut riktig<br />";
       }
+      if (!regexToUse.test(til)) {
+          lovligtil = false;
+          feilmelding = feilmelding + "Flyplasskode er ikke fylt ut riktig<br />";
+      }
 
-  
+
       if (lovligfra) {
+          return true;
+          document.getElementById("melding1").innerHTML = "Alt er riktig fylt ut";
+      } else {
+          document.getElementById("melding1").style.color = "red";
+          document.getElementById("melding1").innerHTML = feilmelding;
+          return false;
+      }
+      if (lovligtil) {
           return true;
           document.getElementById("melding1").innerHTML = "Alt er riktig fylt ut";
       } else {
@@ -55,6 +69,9 @@ function validering() {
       }
 
       if (fra) {
+          document.write("Flyplasskode er korrekt fylt ut ")
+      }
+      if (til) {
           document.write("Flyplasskode er korrekt fylt ut ")
       }
     }
