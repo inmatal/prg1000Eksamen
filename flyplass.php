eeca85eceb5f39affe802e7a44adb1495db2a417
Carl Frost <mosby2@hotmail.com>
2016-12-03 23:27:56 +0100

    <br>

--- flyplass.php
+++ flyplass.php
@@ -80,7 +80,7 @@ if ($fortsett) {
         foreach ($lines as $line) {
             if ($line == $flyplasskode) {
                 $existsAlready = true;
-                $errorMessage = "Flyplasskoden eksisterer!";
+                $errorMessage = "<br>Flyplasskoden eksisterer!";
                 break;
             } else {
                 $fil=fopen($filnavn, $filoperasjon);
