3e20be8298a37a235409a0adbf24e7b2f4140521
staarvik <andreast11@live.no>
2016-12-04 11:31:02 +0100

    fiks

--- visflygninger.php
+++ visflygninger.php
@@ -10,6 +10,26 @@
 
 <body>
 
+
+	  <center><header>Eksamen</header></center>
+
+	<div id="nav">
+	  <ul>
+	    <li><a href="index.html">Gruppe 6 </a> </li>
+	    <li><a href="flyplass.php">Registrere flyplass </a> </li>
+	      <li><a href="visflyplass.php">Vise flyplasser</a> </li>
+	        <li><a href="flyrute.php">Registrere flyrute </a> </li>
+	          <li><a href="visflyrute.php">Vise flyruter</a> </li>
+	            <li><a href="flygning.php">Registrere flygninger</a> </li>
+	              <li><a href="visflygninger.php">Vise flygninger</a> </li>
+	                <li><a href="visavgang.php">Vise avganger</a> </li>
+	                  <li><a href="visankomster.php">Vise ankomster</a> </li>
+
+	  </ul>
+	</div>
+
+
+
 <form method="POST">
 
 	<h4>Trykk fortsett for å se flygninger </br></h4>
@@ -49,4 +69,7 @@ print("</table>");
 
 
 </body>
+<footer>
+<h4>Laget av gruppe 6, som består av:</h4>
+</footer>
 </html>
