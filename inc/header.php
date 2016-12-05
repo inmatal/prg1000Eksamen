a46087a81ec955ce1cbd5da0bac37b1815befb68
Zizstoll <stiankts@gmail.com>
2016-12-05 13:58:01 +0100

    include header

---
+++ inc/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bjarum Airlines</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/design.css">
+    <script type="text/javascript" src="js/hendelser.js"></script>
+    <script type="text/javascript" src="js/hendelser2.js"></script>
+    <script type="text/javascript" src="js/validering.js"></script>
+  </head>
+
+    <body>
+        <div id="wrapper">
+            <center><header>Eksamen</header></center>
+
+            <div id="nav">
+              <ul>
+                <a href="index.html"><li>Forside</li> </a>
+                <a href="flyplass.php"><li>Registrere flyplass</li></a>
+                <a href="visflyplass.php"><li>Vise flyplasser</li></a>
+                <a href="flyrute.php"><li>Registrere flyrute</li></a>
+                <a href="visflyrute.php"><li>Vise flyruter</li></a>
+                <a href="flygning.php"><li>Registrere flygninger</li></a>
+                <a href="visflygninger.php"><li>Vise flygninger</li></a>
+                <a href="visavgang.php"><li>Vise avganger</li></a>
+                <a href="visankomster.php"><li>Vise ankomster</li></a>
+              </ul>
+            </div>
+            <main>
